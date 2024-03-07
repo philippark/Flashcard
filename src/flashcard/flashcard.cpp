580ec41cb615a937de65ef2f2903668b718e54a6
philippark <philippark271@gmail.com>
2024-03-06 22:01:50 -0500

    added dev log and seperated flashcards into flashcard folder

---
+++ src/flashcard/flashcard.cpp
@@ -0,0 +1,21 @@
+#include "flashcard.h"
+
+Flashcard::Flashcard(){
+}
+
+Flashcard::Flashcard(std::string front, std::string back){
+    front_ = front;
+    back_ = back;
+}
+
+std::string Flashcard::get_front(){
+    return front_;
+}
+
+std::string Flashcard::get_back(){
+    return back_;
+}
+
+int Flashcard::get_priority(){
+    return priority_;
+}
